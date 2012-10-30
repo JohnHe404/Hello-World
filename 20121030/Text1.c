00939d40f8137d393f7e1c073a5ecad3796c3a3d
He Minzhang <loongsking@gmail.com>
2012-10-30 21:32:43 +0800

    注释了一些地方，不过中文好像乱码，，

--- 20121030/Text1.c
+++ 20121030/Text1.c
@@ -1,24 +1,24 @@
-#include <reg52.h>
-void lan(void);
+#include <reg52.h> //头文件
+void lan(void);    
 void  main ()
 { 
-   while (1)
+   while (1)    //无限循环
    {
-     P1=0x7f;
+     P1=0x7f;   //0111 1111  控制电流
+	 lan();     //调用延迟函数
+	 P1=0xbf;   //1011 1111
 	 lan();
-	 P1=0xbf;
+	 P1=0xdf;   //1101 1111
 	 lan();
-	 P1=0xdf;
+	 P1=0xef;   //1110 1111
 	 lan();
-	 P1=0xef;
+	 P1=0xf7;   //1111 0111
 	 lan();
-	 P1=0xf7;
+	 P1=0xfb;   //1111 1011
 	 lan();
-	 P1=0xfb;
+	 P1=0xfd;   //1111 1101
 	 lan();
-	 P1=0xfd;
-	 lan();
-	 P1=0xfe;
+	 P1=0xfe;   //1111 1110
 	 lan();
    }
 }
